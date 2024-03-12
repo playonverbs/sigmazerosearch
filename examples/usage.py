e02135c8cbad193d4831b8aea22bdb5ba4a68dd8
Niam Patel <naz1997@googlemail.com>
2024-03-12 16:26:22 +0000

    Update examples/usage.py

--- examples/usage.py
+++ examples/usage.py
@@ -1,4 +1,5 @@
-from ..sigmazerosearch.types.selection import (
+#!/usr/bin/env python3
+from sigmazerosearch.types.selection import (
     Selection,
     Cut,
     Sample,
@@ -8,7 +9,7 @@ from ..sigmazerosearch.types.selection import (
 
 sel = Selection(
     cuts=[
-        Cut("fv", "inFV"),
+        Cut("fv", "InActiveTPC == True"),
         Cut("tracks", "NPrimaryTracklikeDaughters >= 3"),
         Cut("showers", "NPrimaryShowerlikeDaughters >= 1"),
         Cut("muon-id", "..."),
@@ -16,21 +17,35 @@ sel = Selection(
         Cut("photon-bdt", "PhotonBDT > 0.2"),
     ],
     samples=SampleSet(
-        [
-            Sample(
-                "hyperon",
-                "analysisOutputRHC_Overlay_GENIE_Hyperon_cthorpe_make_hyperon_events_numi_rhc_run3b_hyperon_reco2_reco2.root",
-                SampleType.Hyperon,
-                None,  # TODO: fix this to allow getting POT from the file MetaTree
-            ),
-        ]
+        Sample(
+            "hyperon",
+            "/home/niam/phd/data/hyperons/run3b_RHC/analysisOutputRHC_Overlay_GENIE_Hyperon_cthorpe_make_hyperon_events_numi_rhc_run3b_hyperon_reco2_reco2.root",
+            SampleType.Hyperon,
+            None,
+        ),
+        base_dir="/home/niam/phd/data/hyperons/run3b_RHC",
     ),
+    params=[2.0],
 )
 
-for cut in sel.cuts:
-    print(f"Applying {cut.name} Cut")
-    sel.apply_cut(cut.name)
 
-sel.plot_eff_pur()
+def main():
+    # TODO: wrap Selection with a context manager `with open()`-like
+    # needs: sel.__enter__(...) and sel.__exit(...)
+    print("Opening files")
+    sel.open_files()
 
-sel.close_files()
+    for sam in sel.samples:
+        print(sam.file_name)
+    for n, cut in enumerate(sel.cuts[0:1]):
+        print(f"Applying {n} {cut.name} Cut")
+        sel.apply_cut(cut.name)
+        print("...Applied cut")
+
+    # sel.plot_eff_pur()
+    print("Closing files")
+    sel.close_files()
+
+
+if __name__ == "__main__":
+    main()
