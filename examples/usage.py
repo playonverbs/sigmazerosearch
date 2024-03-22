3c5a3cbb9576ac92d99a75e094097abb2013bb76
Niam Patel <naz1997@googlemail.com>
2024-03-22 01:09:19 +0000

    Update examples/usage.py

--- examples/usage.py
+++ examples/usage.py
@@ -6,20 +6,33 @@ from sigmazerosearch.types.selection import (
     SampleSet,
     SampleType,
 )
+import awkward as ak
 
 sel = Selection(
     cuts=[
-        Cut("fv", "InActiveTPC == True"),
-        Cut("tracks", "NPrimaryTracklikeDaughters >= 3"),
-        Cut("showers", "NPrimaryShowerlikeDaughters >= 1"),
-        Cut("muon-id", "..."),
-        Cut("lambda-analysis", "LambdaBDT > 0.4"),
-        Cut("photon-bdt", "PhotonBDT > 0.2"),
+        Cut(
+            "fv",
+            "reco_primary_vtx == True",
+            lambda arr: arr["reco_primary_vtx_inFV"] == True,
+        ),
+        Cut(
+            "tracks",
+            "NPrimaryTracklikeDaughters >= 3",
+            lambda arr: ak.count(arr["pfp_trk_shr_score"] > 0.5, axis=1) >= 3,
+        ),
+        Cut(
+            "showers",
+            "NPrimaryShowerlikeDaughters >= 1",
+            lambda arr: ak.count(arr["pfp_trk_shr_score"] < 0.5, axis=1) >= 1,
+        ),
+        # Cut("muon-id", "..."),
+        # Cut("lambda-analysis", "LambdaBDT > 0.4"),
+        # Cut("photon-bdt", "PhotonBDT > 0.2"),
     ],
     samples=SampleSet(
         Sample(
             "hyperon",
-            "/home/niam/phd/data/hyperons/run3b_RHC/analysisOutputRHC_Overlay_GENIE_Hyperon_cthorpe_make_hyperon_events_numi_rhc_run3b_hyperon_reco2_reco2.root",
+            "/home/niam/phd/data/hyperons/multiSlice/run3b_RHC/analysisOutput_mSlice_cthorpe_make_hyperon_events_numi_rhc_run3b_hyperon_reco2_reco2.root",
             SampleType.Hyperon,
             None,
         ),
@@ -36,11 +49,13 @@ def main():
     sel.open_files()
 
     for sam in sel.samples:
-        print(sam.file_name)
-    for n, cut in enumerate(sel.cuts[0:1]):
-        print(f"Applying {n} {cut.name} Cut")
+        print(sam.file_name.split("/")[-1])
+    for n, cut in enumerate(sel.cuts):
+        print(f"Applying {n}. {cut.name} Cut")
         sel.apply_cut(cut.name)
         print("...Applied cut")
+
+    sel.cut_summary(header=True)
 
     # sel.plot_eff_pur()
     print("Closing files")
