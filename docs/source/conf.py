8c28fcc8efbb092b2904333d323411430b9b5f13
Niam Patel <naz1997@googlemail.com>
2025-11-23 22:43:02 +0000

    chore: update docs copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "sigmazerosearch"
-copyright = "2024, Niam Patel"
+copyright = "2025, Niam Patel"
 author = "Niam Patel"
 
 # -- General configuration ---------------------------------------------------
